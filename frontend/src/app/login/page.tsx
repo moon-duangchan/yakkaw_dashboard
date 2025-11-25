21c58cd3fecf4653119298bdcc2da3ba3647b0e5
moon <moon33120@gmail.com>
2025-11-26 00:19:52 +0700

    ss

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -11,7 +11,7 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Alert, AlertDescription } from "@/components/ui/alert";
-import { api } from "@/utils/api";
+import { api } from "../../../utils/api";
 
 const LoginPage = () => {
   const router = useRouter();
