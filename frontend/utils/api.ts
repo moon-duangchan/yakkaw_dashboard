fc469017a684f02b28f3da8e311682639c36d4f2
moon <moon33120@gmail.com>
2025-11-26 02:04:13 +0700

    remove 8080

--- frontend/utils/api.ts
+++ frontend/utils/api.ts
@@ -1,8 +1,8 @@
 // utils/api.ts
 import axios from "axios";
 
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080";
-export const FRONTEND_BASE_URL = process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+export const FRONTEND_BASE_URL = process.env.NEXT_PUBLIC_FRONTEND_URL;
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
